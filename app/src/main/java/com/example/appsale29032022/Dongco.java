c11042c96d3de419d3b4772a46c128781b64e31b
Phat <phatdroid94@gmail.com>
2022-07-07 19:23:00 +0700

    data structure

--- app/src/main/java/com/example/appsale29032022/Dongco.java
+++
@@ -1,7 +0,0 @@
-package com.example.appsale29032022;
-
-/**
- * Created by pphat on 7/7/2022.
- */
-public class Dongco {
-}
---
+++ app/src/main/java/com/example/appsale29032022/presentation/view/activity/sign_in/SignInActivity.java
@@ -0,0 +1,15 @@
+package com.example.appsale29032022.presentation.view.activity.sign_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.appsale29032022.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
