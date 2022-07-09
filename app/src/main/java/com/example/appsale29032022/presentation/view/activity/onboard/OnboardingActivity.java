10cb5141aad0e110436a8277c391e37d73881e83
Phat <phatdroid94@gmail.com>
2022-07-09 18:39:53 +0700

    update

---
+++ app/src/main/java/com/example/appsale29032022/presentation/view/activity/onboard/OnboardingActivity.java
@@ -0,0 +1,16 @@
+package com.example.appsale29032022.presentation.view.activity.onboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appsale29032022.R;
+
+public class OnboardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
---
+++ app/src/main/java/com/example/appsale29032022/presentation/view/activity/splash/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.appsale29032022.presentation.view.activity.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.appsale29032022.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
