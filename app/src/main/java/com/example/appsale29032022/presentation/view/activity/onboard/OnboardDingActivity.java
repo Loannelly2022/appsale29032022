2ef4b66ed3b5ba2f91b8c9b4604827a1d930e48d
Phat <phatdroid94@gmail.com>
2022-07-12 19:30:11 +0700

    setup viewpager

--- app/src/main/java/com/example/appsale29032022/presentation/view/activity/onboard/OnboardDingActivity.java
+++ app/src/main/java/com/example/appsale29032022/presentation/view/activity/onboard/OnboardDingActivity.java
@@ -2,6 +2,7 @@ package com.example.appsale29032022.presentation.view.activity.onboard;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -18,22 +19,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.appsale29032022.R;
+import com.example.appsale29032022.presentation.view.adapter.OnboardDingPagerAdapter;
 
 public class OnboardDingActivity extends AppCompatActivity {
 
     TextView tvRequestLogin;
     LinearLayout btnGetStarted;
+    ViewPager2 onboardDingViewPager;
+    OnboardDingPagerAdapter onboardDingPagerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
-        tvRequestLogin = findViewById(R.id.textview_request_login);
-        btnGetStarted = findViewById(R.id.button_get_started);
-
+        initial();
         // Request Login Text
         setTextRequestLogin();
     }
+
+    private void initial() {
+        tvRequestLogin = findViewById(R.id.textview_request_login);
+        btnGetStarted = findViewById(R.id.button_get_started);
+        onboardDingViewPager = findViewById(R.id.view_pager_onboard_ding);
+
+        onboardDingPagerAdapter = new OnboardDingPagerAdapter(this);
+        onboardDingViewPager.setAdapter(onboardDingPagerAdapter);
+    }
+
 
     private void setTextRequestLogin() {
         SpannableStringBuilder builder = new SpannableStringBuilder();
