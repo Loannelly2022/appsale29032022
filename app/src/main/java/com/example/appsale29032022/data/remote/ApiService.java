c2665fc8c531571e46e4e8aa8f28f29eb10bbe13
Phat <phatdroid94@gmail.com>
2022-07-14 18:51:38 +0700

    resource data

--- app/src/main/java/com/example/appsale29032022/data/remote/ApiService.java
+++ app/src/main/java/com/example/appsale29032022/data/remote/ApiService.java
@@ -1,7 +1,13 @@
 package com.example.appsale29032022.data.remote;
 
+import retrofit2.Call;
+import retrofit2.http.POST;
+
 /**
  * Created by pphat on 7/14/2022.
  */
 public interface ApiService {
+
+    @POST("user/sign-in")
+    Call<>
 }
